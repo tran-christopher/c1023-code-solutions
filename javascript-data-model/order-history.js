0d3013a0f041323e5b53c8e07a99a1b539e288c0
Christopher Tran <tran.christopher09@gmail.com>
2023-10-24 18:10:37 +0000

    Completed javascrip- dat- model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,71 @@
+const orderHistory = [
+  {
+    orderNumber: 'Order # 114-3941689-8772232',
+    orderPlaced: 'ORDER PLACED August 4, 2020',
+    orderDelivered: 'Delivered Aug 8, 2020',
+    total: 'TOTAL $34',
+    recipient: 'SHIP TO JS Masher',
+    item1: [
+      {
+        item: 'Javascript for impatient programmers',
+        itemDescription:
+          'Rauschmayer, Dr. Axel Return window closed on Sep 7, 2020',
+        itemPrice: 31.55,
+      },
+    ],
+  },
+
+  {
+    orderNumber: 'ORDER 113-9984268-1280257',
+    orderPlaced: 'ORDER PLACED July 19, 2020',
+    orderDelivered: 'Delivered Jul 20, 2020',
+    total: 'TOTAL $44.53',
+    recipient: 'SHIP TO JS Masher',
+    item1: [
+      {
+        item: 'The Timeless Way of Building',
+        itemDescription:
+          'Alexander, Christopher Return window closed on Aug 19, 2020',
+        itemPrice: 41.33,
+      },
+    ],
+  },
+
+  {
+    orderNumber: 'ORDER 114-2875557-9059409',
+    orderPlaced: 'ORDER PLACED July 4, 2020',
+    orderDelivered: 'Delivered Jul 7, 2020',
+    total: 'TOTAL $17.22',
+    recipient: 'SHIP TO JS Masher',
+    item1: [
+      {
+        item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        itemDescription: 'Return window closed on Aug 5, 2020',
+        itemPrice: 15.98,
+      },
+    ],
+  },
+
+  {
+    orderNumber: 'ORDER 113-2883177-2648248',
+    orderPlaced: 'ORDER PLACED 7/3/2020',
+    orderDelivered: 'Delivered 7/5/2020',
+    total: 'TOTAL $138.93',
+    recipient: 'SHIP TO JS Masher',
+    item1: [
+      {
+        item: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        itemDescription: 'Return window closed on Aug 4, 2020',
+        itemPrice: 94.95,
+      },
+    ],
+    item2: [
+      {
+        item: 'The Art of Sql',
+        itemDescription:
+          'Faroult, Stephane Return window closed on Aug 4, 2020',
+        itemPrice: 33.99,
+      },
+    ],
+  },
+];
