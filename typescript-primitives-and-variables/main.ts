a5cf82b890a08510d3b778bf853e66f500fac363
Christopher Tran <tran.christopher09@gmail.com>
2023-12-06 23:02:16 +0000

    Completed typescript-primitives-and-variables

---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,19 @@
+const fullName: string = 'string';
+const isCool: boolean = false;
+const totalPets: number = 0;
+
+console.log('fullName:', fullName);
+console.log('isCool:', isCool);
+console.log('totalPets:', totalPets);
+
+console.log('type of fullName:', typeof fullName);
+console.log('type of isCool:', typeof isCool);
+console.log('type of totalPets:', typeof totalPets);
+
+const unknown: null = null;
+console.log('unknown:', unknown);
+console.log('type of unknown:', typeof unknown);
+
+const never: undefined;
+console.log('never:', never);
+console.log('type of never:', typeof never);
