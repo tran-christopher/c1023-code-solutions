aa6cb019ed5a0bbea542afec653692bae461d547
Christopher Tran <tran.christopher09@gmail.com>
2023-12-15 00:30:15 +0000

    Working on 1.5

--- practice-exercises/01-basic-challenges-1/05-find-max-number/find-max-number.ts
+++ practice-exercises/01-basic-challenges-1/05-find-max-number/find-max-number.ts
@@ -1,3 +1,12 @@
 export function findMaxNumber(arr: number[]): number {
-  return 0;
+  let largest: number = 0;
+  arr.forEach((element) => {
+    switch (element > largest) {
+      case true:
+        largest = element;
+        break;
+      default:
+    }
+  });
+  return largest;
 }
