f9b04aec83ab4ae7d026a54ca6897a225bef177c
Christopher Tran <tran.christopher09@gmail.com>
2023-12-15 17:44:37 +0000

    Completed express-get-json

---
+++ express-get-json/server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+
+const app = express();
+
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
+
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = [];
+  for (const key in grades) {
+    const objToPush = grades[key];
+    gradesArray.push(objToPush);
+  }
+  res.jsonp(gradesArray);
+});
+
+app.listen(8080, () => {
+  console.log('server is listening!');
+});
