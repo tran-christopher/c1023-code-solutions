02f801e1be308d12870124655b59d81a27fbebb8
Christopher Tran <tran.christopher09@gmail.com>
2023-12-17 01:30:55 +0000

    Improved code using recommendations

--- node-module-system/calculate.ts
+++ node-module-system/calculate.ts
@@ -8,20 +8,29 @@ const arg3: string = process.argv[3];
 const arg4: string = process.argv[4];
 
 if (arg3 === 'plus') {
+  addNumbers();
+} else if (arg3 === 'minus') {
+  subtractNumbers();
+} else if (arg3 === 'times') {
+  multiplyNumbers();
+} else if (arg3 === 'over') {
+  divideNumbers();
+} else {
+  console.log('Invalid arugments');
+}
+
+function divideNumbers(): void {
+  console.log('result:', divide(Number(arg2), Number(arg4)));
+}
+
+function multiplyNumbers(): void {
+  console.log('result:', multiply(Number(arg2), Number(arg4)));
+}
+
+function subtractNumbers(): void {
+  console.log('result:', subtract(Number(arg2), Number(arg4)));
+}
+
+function addNumbers(): void {
   console.log('result:', add(Number(arg2), Number(arg4)));
-} else if (process.argv[3] === 'minus') {
-  console.log(
-    'result:',
-    subtract(Number(process.argv[2]), Number(process.argv[4]))
-  );
-} else if (process.argv[3] === 'times') {
-  console.log(
-    'result:',
-    multiply(Number(process.argv[2]), Number(process.argv[4]))
-  );
-} else if (process.argv[3] === 'over') {
-  console.log(
-    'result:',
-    divide(Number(process.argv[2]), Number(process.argv[4]))
-  );
 }
