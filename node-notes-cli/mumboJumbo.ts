54958ded89aec8a93f034f73d3674e0a10149ef8
Christopher Tran <tran.christopher09@gmail.com>
2023-12-17 02:23:52 +0000

    Extracted resued code into functions, took out unnecessary loop

--- node-notes-cli/mumboJumbo.ts
+++ node-notes-cli/mumboJumbo.ts
@@ -10,24 +10,21 @@ const [, , command, string1, string2] = process.argv;
 try {
   switch (command) {
     case 'read': {
-      const data = await test();
+      const data = await read();
       for (const key in data.notes) {
         console.log(`${key} - ${data.notes[key]}`);
       }
       break;
     }
     case 'add': {
-      let objJson = await readFile('data.json', { encoding: 'utf-8' });
-      const objJsonParsed = JSON.parse(objJson);
+      const objJsonParsed = await getObject();
       objJsonParsed.notes[objJsonParsed.nextId.toString()] = string1;
       objJsonParsed.nextId = objJsonParsed.nextId + 1;
-      objJson = JSON.stringify(objJsonParsed, null, 2);
-      await writeFile('data.json', objJson, { encoding: 'utf-8' });
+      await newObject(objJsonParsed);
       break;
     }
     case 'delete': {
-      let objJson = await readFile('data.json', { encoding: 'utf-8' });
-      const objJsonParsed = JSON.parse(objJson);
+      const objJsonParsed = await getObject();
       for (const key in objJsonParsed.notes) {
         switch (key) {
           case string1: {
@@ -37,24 +34,13 @@ try {
           default:
         }
       }
-      objJson = JSON.stringify(objJsonParsed, null, 2);
-      await writeFile('data.json', objJson, { encoding: 'utf-8' });
+      await newObject(objJsonParsed);
       break;
     }
     case 'update': {
-      let objJson = await readFile('data.json', { encoding: 'utf-8' });
-      const objJsonParsed = JSON.parse(objJson);
-      for (const key in objJsonParsed.notes) {
-        switch (key) {
-          case string1: {
-            objJsonParsed.notes[string1] = string2;
-            break;
-          }
-          default:
-        }
-      }
-      objJson = JSON.stringify(objJsonParsed, null, 2);
-      await writeFile('data.json', objJson, { encoding: 'utf-8' });
+      const objJsonParsed = await getObject();
+      objJsonParsed.notes[string1] = string2;
+      await newObject(objJsonParsed);
       break;
     }
     default:
@@ -64,8 +50,23 @@ try {
   console.log(e instanceof Error);
 }
 
-async function test(): Promise<Data> {
+async function read(): Promise<Data> {
   const contents = await readFile('data.json', { encoding: 'utf-8' });
   const data = JSON.parse(contents);
   return data;
+}
+
+async function getObject(): Promise<Data> {
+  const objJson = await readFile('data.json', { encoding: 'utf-8' });
+  const objJsonParsed = JSON.parse(objJson);
+  return objJsonParsed;
+}
+
+async function newObject(jsonObject: Data): Promise<void> {
+  try {
+    const objJsonReturn = JSON.stringify(jsonObject, null, 2);
+    await writeFile('data.json', objJsonReturn, { encoding: 'utf-8' });
+  } catch (e) {
+    console.log('Something went wrong in newObject()!:', e);
+  }
 }
