ab4f2fa40b7508bf45f88f5e283b0b6e10ebca43
Christopher Tran <tran.christopher09@gmail.com>
2024-01-11 00:12:14 +0000

    Completed react-carousel-component

--- react-carousel-component/my-app/src/CarouselWrapper.tsx
+++ react-carousel-component/my-app/src/CarouselWrapper.tsx
@@ -1,70 +1,77 @@
-// // import { useEffect, useState } from 'react';
-// import { Indicators } from './Indicators';
-// import { Banner } from './Banner';
-// // import { NextButton } from './NextButton';
-// import { PrevButton } from './PrevButton';
+import { useCallback, useEffect, useState } from 'react';
+import { Indicators } from './Indicators';
+import { Banner } from './Banner';
+import { NextButton } from './NextButton';
+import { PrevButton } from './PrevButton';
 
-// type Image = {
-//   src: string;
-//   alt: string;
-// };
+type Image = {
+  src: string;
+  alt: string;
+};
 
-// type Props = {
-//   images: Image[];
-// };
+type Props = {
+  images: Image[];
+};
 
-// export function CarouselWrapper({ images }: Props) {
-//   // const [index, setIndex] = useState(0);
+export function CarouselWrapper({ images }: Props) {
+  const [index, setIndex] = useState(0);
+  const handleCarousel = useCallback(() => {
+    if (index === 5) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }, [index]);
 
-//   // useEffect(() => {
-//   //   const interval = setInterval(() => {
-//   //     console.log('running');
-//   //     handleNext();
-//   //   }, 2500);
-//   //   return () => clearInterval(interval);
-//   // }, [handleNext]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('running');
+      handleCarousel();
+    }, 2500);
+    return () => clearInterval(interval);
+  }, [handleCarousel]);
 
-//   // function handleNext() {
-//   //   if (index === 5) {
-//   //     setIndex(0);
-//   //   } else {
-//   //     setIndex(index + 1);
-//   //   }
-//   // }
+  function handleNext() {
+    if (index === 5) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
 
-//   function handlePrev() {
-//     if (index === 0) {
-//       setIndex(5);
-//     } else {
-//       setIndex(index - 1);
-//     }
-//   }
+  function handlePrev() {
+    if (index === 0) {
+      setIndex(5);
+    } else {
+      setIndex(index - 1);
+    }
+  }
 
-//   function handleAccordion(clickedIndex: number) {
-//     setIndex(clickedIndex);
-//   }
+  function handleAccordion(clickedIndex: number) {
+    setIndex(clickedIndex);
+  }
 
-//   return (
-//     <div
-//       style={{
-//         height: '70vh',
-//         width: '100%',
-//         backgroundColor: 'white',
-//       }}>
-//       <div
-//         style={{
-//           display: 'flex',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//           paddingTop: '10%',
-//         }}>
-//         <PrevButton onClick={handlePrev} />
-//         <Banner src={images[index].src} alt={images[index].alt} />
-//         {/* <NextButton onClick={handleNext} /> */}
-//       </div>
-//       <div style={{}}>
-//         <Indicators onClick={handleAccordion} current={index} />
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div
+      style={{
+        height: '70vh',
+        width: '100%',
+        backgroundColor: 'white',
+      }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingTop: '10%',
+        }}>
+        <PrevButton onClick={handlePrev} />
+        <Banner src={images[index].src} alt={images[index].alt} />
+        <NextButton onClick={handleNext} />
+      </div>
+      <div style={{}}>
+        <Indicators onClick={handleAccordion} current={index} />
+      </div>
+    </div>
+  );
+}
