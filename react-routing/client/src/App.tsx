bff4e9ad6b79a477aa871b7af34713412ceceeea
Christopher Tran <tran.christopher09@gmail.com>
2024-01-12 18:42:22 +0000

    Completed react-routing

---
+++ react-routing/client/src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { Catalog } from './pages/Catalog';
+import { ProductDetails } from './pages/ProductDetails';
+import { About } from './pages/About';
+import { NotFound } from './pages/NotFound';
+
+export function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Catalog />} />
+          <Route
+            path="details/:productId"
+            loader={({ params }): string => {
+              return params.productId;
+            }}
+            element={<ProductDetails />}
+          />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
