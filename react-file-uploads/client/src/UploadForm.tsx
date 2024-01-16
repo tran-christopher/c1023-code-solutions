ae2fd206308788ba319a43c0108aef6c0f6f931c
Christopher Tran <tran.christopher09@gmail.com>
2024-01-16 18:40:53 +0000

    Committing to restart computer

---
+++ react-file-uploads/client/src/UploadForm.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove me */
+import { type FormEvent, useState } from 'react';
+
+type Image = {
+  imageId: number;
+  url: string;
+  caption: string;
+};
+
+export function UploadForm() {
+  const [imageFile, setImageFile] = useState('');
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    try {
+      event.preventDefault();
+      const formData = new FormData(event.currentTarget);
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(formData),
+      };
+      const response = await fetch('/api/uploads', options);
+      if (!response.ok) throw new Error(`Server error: ${response.status}`);
+      const newImage = await response.json();
+      console.log(newImage);
+      setImageFile(newImage);
+    } catch (error) {
+      console.error(error);
+    }
+
+    /* Prevent the browser's default behavior for form submissions.
+     * Create a `new` FormData object from the `event`.
+     *
+     * Use fetch() to send a POST request to /api/uploads.
+     * The body should be the form data object you created (not a JSON string).
+     * Headers are not necessary as fetch will use the correct Content-Type
+     * automatically (multipart/form-data).
+     *
+     * Parse the JSON response body and log the parsed response body.
+     * Set the `imageFile` state to the parsed response body.
+     * Log any errors to the console (using `console.error`).
+     *
+     * References:
+     * https://developer.mozilla.org/en-US/docs/Web/API/FormData/FormData
+     * https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#uploading_a_file
+     */
+  }
+
+  return (
+    <div className="container">
+      <div className="row min-vh-100 pb-5 justify-content-center align-items-center">
+        <div className="col col-md-8">
+          <h3 className="text-center mb-5">React File Uploads</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label className="form-label">
+                Caption:
+                <input
+                  required
+                  autoFocus
+                  type="text"
+                  id="caption"
+                  name="caption"
+                  className="form-control bg-light"
+                />
+              </label>
+            </div>
+            <div className="d-flex justify-content-between align-items-center">
+              <input
+                required
+                type="file"
+                name="image"
+                accept=".png, .jpg, .jpeg, .gif"
+              />
+              <button type="submit" className="btn btn-primary">
+                Upload
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
